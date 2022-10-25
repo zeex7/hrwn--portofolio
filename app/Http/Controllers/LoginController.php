f83350edc2532cc185ea42c2464c58800855605a
zeex7 <herwanramadhani11@gmail.com>
2022-10-25 13:28:29 +0700

    perbaikan login

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Auth;
 class LoginController extends Controller
 {
     public function index(){
-        return view('Login');
+        return view('login');
     }
 
     public function authenticate(Request $request){
