0e0204afcbd4c77c4ba0d549ae139bd5bf026d13
zeex7 <herwanramadhani11@gmail.com>
2022-10-25 13:40:24 +0700

    zee

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -29,13 +29,13 @@
         <!-- Outer Row -->
         <div class="row justify-content-center">
 
-            <div class="col-xl-7 col-lg-12 col-md-9">
+            <div class="col-xl-10 col-lg-12 col-md-9">
 
                 <div class="card o-hidden border-0 shadow-lg my-5">
                     <div class="card-body p-0">
                         <!-- Nested Row within Card Body -->
                         <div class="row">
-                            {{-- <div class="col-lg-6 d-none d-lg-block bg-login-image"></div> --}}
+                            <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
                             <div class="col-lg">
                                 <div class="p-5">
                                     <div class="text">
