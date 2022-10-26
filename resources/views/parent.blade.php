b5ce3537e908370f6f2307987865fe93ca4ee148
zeex7 <herwanramadhani11@gmail.com>
2022-10-26 09:26:40 +0700

    Login Kontol

--- resources/views/parent.blade.php
+++ resources/views/parent.blade.php
@@ -52,6 +52,9 @@
             <li class="nav-item">
               <a class="nav-link active" href="/contact">Contact</a>
             </li>
+            <li class="nav-item">
+              <a class="nav-link active" href="/Login">Login</a>
+            </li>
           </ul>
         </div>
       </div>
