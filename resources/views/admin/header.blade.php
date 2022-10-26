d1dbea31e4fd3b61d1e69d95c900c57cfa992c21
zeex7 <herwanramadhani11@gmail.com>
2022-10-26 09:38:05 +0700

    lahyo

--- resources/views/admin/header.blade.php
+++ resources/views/admin/header.blade.php
@@ -5,7 +5,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
     <!-- Custom fonts for this template-->
-    <link href= {{ asset ('template/vendor/fontawesome-free/css/all.min.css') }} rel="stylesheet" type="text/css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" rel="stylesheet" type="text/css">
     <link
         href= {{ asset ('template/https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i') }}
         rel="stylesheet">
